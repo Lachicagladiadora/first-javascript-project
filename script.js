692b8befb6095a8de324459c8d2c5847829ef35c
Lachicagladiadora <200379@unsaac.edu.pe>
2024-06-18 21:36:14 -0500

    feat: add functions in js and template in html

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('Hello, world');
+
+const getAddition = (a, b) => {
+ const addition = a+b
+ return addition
+}
+
+const getSubtract = (a,b) => {
+ const subtract = a-b
+return subtract
+}
+
+const getMultiplication = (a,b) =>{
+  const multiplication = a * b
+  return multiplication
+}
+
+const getDivision = (a,b)=>{
+  const division = a/b
+  return division
+}
