9d28b72259fb76d7912480d6c77908c2a3b60462
Lachicagladiadora <200379@unsaac.edu.pe>
2024-07-29 19:33:11 -0500

    chore: advance onchange input

--- script.js
+++ script.js
@@ -14,10 +14,18 @@ const number8ButtonElement = document.getElementById("number8")
 const number9ButtonElement = document.getElementById("number9")
 const number0ButtonElement = document.getElementById("number0")
 
+const additionButtonElement = document.getElementById("addition")
+const subtractionButtonElement = document.getElementById("subtraction")
+const multiplicationButtonElement = document.getElementById("multiplication")
+const divisionButtonElement = document.getElementById("division")
+
+const deleteButtonElement = document.getElementById("delete")
+const cleanButtonElement = document.getElementById("clean")
+
 if(!inputDisplayElement)console.warn('not found "id" from "inputDisplayElement"')
 if(!calculateButtonElement)console.warn('not found "id" from "calculateButtonElement"')
 if(!displayElement)console.warn('not found "id" from "displayElement"')
-  
+
 if(!number1ButtonElement)console.warn('not found "id" from "number1ButtonElement"')
 if(!number2ButtonElement)console.warn('not found "id" from "number2ButtonElement"')
 if(!number3ButtonElement)console.warn('not found "id" from "number3ButtonElement"')
@@ -29,14 +37,25 @@ if(!number8ButtonElement)console.warn('not found "id" from "number8ButtonElement
 if(!number9ButtonElement)console.warn('not found "id" from "number9ButtonElement"')
 if(!number0ButtonElement)console.warn('not found "id" from "number0ButtonElement"')
 
+if(!additionButtonElement) console.warn('not found "id" from "addition"')
+if(!subtractionButtonElement) console.warn('not found "id" from "subtraction"')
+if(!multiplicationButtonElement) console.warn('not found "id" from "multiplication"')
+if(!divisionButtonElement) console.warn('not found "id" from "division"')
+  
+if(!deleteButtonElement)console.warn('not found "id" from "delete"')
+if(!cleanButtonElement)console.warn('not found "id" from "clean"')
+
+const INPUT_VALUE = [""]
+console.log({INPUT_VALUE}, '1')
+  
 const getAddition = (a, b) => {
  const addition = a+b
  return addition
 }
 
 const getSubtract = (a,b) => {
- const subtract = a-b
-return subtract
+  const subtract = a-b
+  return subtract
 }
 
 const getMultiplication = (a,b) =>{
@@ -54,40 +73,98 @@ const getDivision = (a,b)=>{
   // const valueconst = {'pi': 22/7, 'e': 2.718}
 
 const onChange = (e)=>{
-  const value = e.target.value
-  console.log({value})
+  console.log({e}, "event", e.data)
+  const possibleValue = (e.target.value)
+  console.log({possibleValue})
+  onWrite(possibleValue)
+  
+  console.log({INPUT_VALUE})
+  // return INPUT_VALUE
 }
 
-const writeNumber=(e)=>{
-  value = e.target
-  console.log('number', e, {value})
+// const onFocus = (e)=>{
+//   console.log({e})
+//   e.isTrusted = true
+// }
+
+const onWrite=(value)=>{
+// if(!INPUT_VALUE) {inputDisplayElement.value = value}
+// else{
+  INPUT_VALUE.push(value)
+  inputDisplayElement.value = "".concat(...INPUT_VALUE)
+  
+  console.log({INPUT_VALUE}, '3')
+
+// }
+
+  // console.log('number', e, {value})
+  // onFocus(e)
+  // onChange(e)
 }
 
-const onSubmit = (e)=> {
-  e.preventDefault()
-  const input = displayElement.get(inputDisplayElement)
-  console.log({input})
+const onSubmit = ()=> {
+  // e.preventDefault()
+  console.log("fui enviado")
+  // const input = displayElement.get(inputDisplayElement)
+  // console.log({input})
 }
 
-const getResult=(e)=>{
+const getResult=()=>{
+  inputDisplayElement.value = "wally"
+console.log({INPUT_VALUE}, '4')
+
   console.log('result')
-  onSubmit(e)
+  // onSubmit(e)
 }
 
+// que en el input se escriba lo que se presiona con los botones
 
-inputDisplayElement.addEventListener('input', (e)=>onChange(e))
+const onDelete= ()=>{
+  INPUT_VALUE.pop()
+  inputDisplayElement.value = "".concat(...INPUT_VALUE)
 
-number1ButtonElement.addEventListener('keydown', (e)=>{
-  if(e.value === 1) return 1
-  console.log({e})
-})
+}
 
-number2ButtonElement.addEventListener('keydown', (e)=>writeNumber(e))
-number3ButtonElement.addEventListener('keydown', (e)=>writeNumber(e))
-number4ButtonElement.addEventListener('keydown', (e)=>writeNumber(e))
-number5ButtonElement.addEventListener('keydown', (e)=>writeNumber(e))
-number6ButtonElement.addEventListener('keydown', (e)=>writeNumber(e))
-number7ButtonElement.addEventListener('keydown', (e)=>writeNumber(e))
-number8ButtonElement.addEventListener('keydown', (e)=>writeNumber(e))
-number9ButtonElement.addEventListener('keydown', (e)=>writeNumber(e))
-number0ButtonElement.addEventListener('keydown', (e)=>writeNumber(e))
+const onClean = ()=>{
+  const lenght = INPUT_VALUE.length
+  if (lenght){
+    for(let i = 0; i < lenght; i++){
+      INPUT_VALUE.pop()
+    }
+    inputDisplayElement.value = ''.concat(...INPUT_VALUE)
+  }
+  inputDisplayElement.value = ''.concat(...INPUT_VALUE)
+}
+
+// que no se pierda foco del input al presionar fuera de este
+// hacer que funcionen los botones para escibir en el input
+
+
+// que funcione el boton de borrar
+// que funcione el boton igual
+// que funcione el boton de limpiar input
+
+// que sume decimales
+
+// inputDisplayElement.value = value
+inputDisplayElement.addEventListener("input", (e)=>onChange(e))
+
+number0ButtonElement.addEventListener("click", ()=>onWrite('0'))
+number1ButtonElement.addEventListener("click", ()=>onWrite('1'))
+number2ButtonElement.addEventListener("click", ()=>onWrite('2'))
+number3ButtonElement.addEventListener("click", ()=>onWrite('3'))
+number4ButtonElement.addEventListener("click", ()=>onWrite('4'))
+number5ButtonElement.addEventListener("click", ()=>onWrite('5'))
+number6ButtonElement.addEventListener("click", ()=>onWrite('6'))
+number7ButtonElement.addEventListener("click", ()=>onWrite('7'))
+number8ButtonElement.addEventListener("click", ()=>onWrite('8'))
+number9ButtonElement.addEventListener("click", ()=>onWrite('9'))
+
+additionButtonElement.addEventListener("click", ()=>onWrite('+'))
+subtractionButtonElement.addEventListener("click", ()=>onWrite('-'))
+multiplicationButtonElement.addEventListener("click", ()=>onWrite('×'))
+divisionButtonElement.addEventListener("click", ()=>onWrite('÷'))
+
+deleteButtonElement.addEventListener("click", onDelete)
+calculateButtonElement.addEventListener("click", getResult)
+cleanButtonElement.addEventListener("click", onClean)
